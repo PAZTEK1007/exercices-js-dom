192b68795617b8901b3e7376cbdbb54ade209c84
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-18 16:29:46 +0200

    push exercices 2 files

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,15 +8,30 @@ const imgImportant = document.querySelectorAll("img");
 
 imgImportant.forEach(imgImportant => {
     if (imgImportant.classList.contains('important')) {
+        imgImportant.style.display = "block";
+    } else {
         imgImportant.style.display = "none";
     }
 });
 
 const paragraph = document.querySelectorAll("p");
 
-paragraph.forEach(paragraph => {
-    console
-    if (paragraph.classList.contains()){
-        console.log(paragraph.className);
-    }
+paragraph.forEach((paragraph, index ) => {
+   const text = paragraph.textContent;
+   const className = paragraph.className;
+
+   const rgb = [];
+   for (let i = 0; i < 3; i++) {
+       let random = Math.floor(Math.random()*256)
+       rgb.push(random)
+   }
+   const rgbToString = ` rgb(${rgb.join()})`;
+    paragraph.style.color = rgbToString;
+
+   console.log(`Paragraph ${index + 1}: ${text}`);
+   
+   if (className) {
+    paragraph.style.color = "black";
+    console.log(` Class Name : ${className}`);
+   }
 });
