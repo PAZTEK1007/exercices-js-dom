4d96420fc227fb7e93c095b54b6820d1aa2dadea
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-22 17:20:16 +0200

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,25 +5,41 @@ const getElapsedTime = () => {
 }
 
 const clickOnSquare = (e) => {
-  console.log(e.target.classList[1])
-  console.log(getElapsedTime())
-}
-function displaySquare(){
+  console.log(e.target.classList[1]);
+  console.log(getElapsedTime());
+  const colorClass = e.target.classList[1];
+  const square = document.createElement("div");
+  square.classList.add("displayedsquare", colorClass);
+  document.querySelector(".displayedsquare-wrapper").appendChild(square);
 
-const wrapper = document.getElementsByClassName("displayedsquare-wrapper");
-const firstChild = wrapper.firstChild;
-const green = document.createElement("div", {"class":"displayedsquare green"});
-const violet = document.createElement("div", {"class":"displayedsquare violet"});
-const orange = document.createElement("div", {"class":"displayedsquare green"});
-firstChild.appendChild(orange);
-firstChild.appendChild(violet);
-firstChild.appendChild(green);
+  const ul = document.querySelector("ul");
+  const list = document.createElement("li");
+  const listContent = document.createTextNode(
+    `[${getElapsedTime()}] You create a ${e.target.classList[1]} square !`
+  );
+  list.appendChild(listContent);
+  ul.appendChild(list);
 
 }
 
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
-  actionSquare.addEventListener('click', clickOnSquare, displaySquare)
-  
+  actionSquare.addEventListener('click', clickOnSquare, );
 }
+
+function randomColor(){
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  }
+const body = document.getElementsByTagName("body");
+body.addEventListener('keydown', function(event) {
+    if (event.key === ' ' || event.keyCode === 32) {
+        body.style.backgroundColor= randomColor();
+    }
+});
+
+
 
